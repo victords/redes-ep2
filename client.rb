06324b1d80f959bf27896761b046733e411c4155
Victor David Santos <victordavidsantos@gmail.com>
2014-09-21 15:30:08 -0300

    Servidor aceitando conexões TCP e UDP; Cliente pode iniciar em modo TCP ou UDP.

--- client.rb
+++ client.rb
@@ -1,9 +1,32 @@
 require 'socket'
 
-s = TCPSocket.new ARGV.shift, ARGV.shift.to_i
-until s.closed?
-	s.print gets
-	puts s.readline
-	if s.eof?; s.close
-	else; s.readline; end
+if ARGV.length < 3
+	puts "Usage: client.rb HOST PORT MODE"
+	puts "  HOST is the hostname or address of the server."
+	puts "  PORT is the number of the port where the server is listening for connections."
+	puts "  MODE must be either 'tcp' or 'udp'."
+	exit 0
+end
+
+host = ARGV.shift
+port = ARGV.shift.to_i
+mode = ARGV.shift.downcase
+
+if mode == "udp"
+	s = UDPSocket.new
+	s.connect host, port
+	until s.closed?
+		s.print gets
+		msg, sender = s.recvfrom(30)
+		puts msg
+		s.close if msg.chomp == "Bye!"
+	end
+else
+	s = TCPSocket.new host, port
+	until s.closed?
+		s.print gets
+		puts s.readline
+		if s.eof?; s.close
+		else; s.readline; end
+	end
 end
--- server.rb
+++ server.rb
@@ -1,6 +1,29 @@
 require 'socket'
 
-server = TCPServer.open(ARGV[0])
+if ARGV.length < 1
+	puts "Usage: server.rb PORT"
+	puts "  PORT is the number of the port where the server will listen for connections."
+	exit 0
+end
+
+port = ARGV.shift.to_i
+
+# Inicializando UDP
+Thread.start do
+	server = UDPSocket.new
+	server.bind nil, port
+	loop {
+		msg, sender = server.recvfrom(10)
+		if msg.chomp == "exit"
+			server.send "Bye!\n", 0, sender[3], sender[1]
+		else
+			server.send "You typed: #{msg}", 0, sender[3], sender[1]
+		end
+	}
+end
+
+# Inicializando TCP
+server = TCPServer.open(port)
 loop {
 	Thread.start(server.accept) do |client|
 		puts "aceitou conexão"
